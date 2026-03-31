620f9cfec9bcbd5ec1878376214a6428271e231d
nengida1 <nengida@seas.upenn.edu>
2026-03-31 01:34:51 -0400

    Update Destination.sol

--- Destination.sol
+++ Destination.sol
@@ -8,57 +8,65 @@ import "./BridgeToken.sol";
 contract Destination is AccessControl {
     bytes32 public constant WARDEN_ROLE = keccak256("BRIDGE_WARDEN_ROLE");
     bytes32 public constant CREATOR_ROLE = keccak256("CREATOR_ROLE");
-	mapping( address => address) public underlying_tokens;
-	mapping( address => address) public wrapped_tokens;
-	address[] public tokens;
 
-	event Creation( address indexed underlying_token, address indexed wrapped_token );
-	event Wrap( address indexed underlying_token, address indexed wrapped_token, address indexed to, uint256 amount );
-	event Unwrap( address indexed underlying_token, address indexed wrapped_token, address frm, address indexed to, uint256 amount );
+    // wrapped => underlying
+    mapping(address => address) public underlying_tokens;
 
-    constructor( address admin ) {
+    // underlying => wrapped
+    mapping(address => address) public wrapped_tokens;
+
+    address[] public tokens;
+
+    event Creation(address indexed underlying_token, address indexed wrapped_token);
+    event Wrap(address indexed underlying_token, address indexed wrapped_token, address indexed to, uint256 amount);
+    event Unwrap(address indexed underlying_token, address indexed wrapped_token, address frm, address indexed to, uint256 amount);
+
+    constructor(address admin) {
         _grantRole(DEFAULT_ADMIN_ROLE, admin);
         _grantRole(CREATOR_ROLE, admin);
         _grantRole(WARDEN_ROLE, admin);
     }
 
-	function wrap(address _underlying_token, address _recipient, uint256 _amount ) public onlyRole(WARDEN_ROLE) {
-		//YOUR CODE HERE
-    address wrapped = underlying_tokens[_underlying_token];
-    require(wrapped != address(0), "token not registered");
+    function wrap(address _underlying_token, address _recipient, uint256 _amount)
+        public
+        onlyRole(WARDEN_ROLE)
+    {
+        address wrapped = wrapped_tokens[_underlying_token];
+        require(wrapped != address(0), "token not registered");
 
-    emit Wrap(_underlying_token, wrapped, _recipient, _amount);
-	BridgeToken(wrapped).mint(_recipient, _amount);
+        emit Wrap(_underlying_token, wrapped, _recipient, _amount);
+        BridgeToken(wrapped).mint(_recipient, _amount);
+    }
 
-    
-	}
+    function unwrap(address _wrapped_token, address _recipient, uint256 _amount) public {
+        address underlying = underlying_tokens[_wrapped_token];
+        require(underlying != address(0), "token not registered");
 
-	function unwrap(address _wrapped_token, address _recipient, uint256 _amount ) public {
-		//YOUR CODE HERE
-    address underlying = wrapped_tokens[_wrapped_token];
-    require(underlying != address(0), "token not registered");
+        emit Unwrap(underlying, _wrapped_token, msg.sender, _recipient, _amount);
+        BridgeToken(_wrapped_token).burn(_amount);
+    }
 
-    emit Unwrap(underlying, _wrapped_token, msg.sender, _recipient, _amount);
-	BridgeToken(_wrapped_token).burn(_amount);
+    function createToken(address _underlying_token, string memory name, string memory symbol)
+        public
+        onlyRole(CREATOR_ROLE)
+        returns (address)
+    {
+        require(wrapped_tokens[_underlying_token] == address(0), "token already exists");
 
-    
-	}
+        BridgeToken wrapped = new BridgeToken(
+            _underlying_token,
+            name,
+            symbol,
+            address(this)
+        );
 
-	function createToken(address _underlying_token, string memory name, string memory symbol ) public onlyRole(CREATOR_ROLE) returns(address) {
-		//YOUR CODE HERE
-    require(underlying_tokens[_underlying_token] == address(0), "token already exists");
+        wrapped_tokens[_underlying_token] = address(wrapped);
+        underlying_tokens[address(wrapped)] = _underlying_token;
 
-    BridgeToken wrapped = new BridgeToken(_underlying_token, name, symbol, address(this));
+        // try storing the underlying token in the registry list
+        tokens.push(_underlying_token);
 
-    underlying_tokens[_underlying_token] = address(wrapped);
-    wrapped_tokens[address(wrapped)] = _underlying_token;
-    tokens.push(address(wrapped));
-
-    emit Creation(_underlying_token, address(wrapped));
-
-    return address(wrapped);
-	}
-
+        emit Creation(_underlying_token, address(wrapped));
+        return address(wrapped);
+    }
 }
-
-
